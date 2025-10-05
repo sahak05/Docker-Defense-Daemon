e31efd02e21345ab2c77d58767af850705729ce0
Abdoul Sadikou <sadikouhak@gmail.com>
2025-10-05 14:53:39 -0400

    init

---
+++ daemon/app.py
@@ -0,0 +1,12 @@
+import time 
+from flask import Flask, request, jsonify
+import docker
+import subprocess
+import json
+
+app = Flask(__name__)
+
+
+if __name__ == "__main__"
+import threading
+app.run(host="0.0.0.0", port=8080)
